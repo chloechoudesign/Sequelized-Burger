97beca726f22b72eeff00f2351fe0f8a7e5830d0
Your NameChloe <chloechoudesign@gmail.com>
2017-10-05 13:33:47 -0700

    ready to push

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,58 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+  // GET route 
+  app.get("/", function(req, res) {
+
+    db.Burger.findAll({}).then(function(dbBurger) {
+      // console.log(dbBurger);
+      // res.json(dbBurger);
+      var hbsObject = {
+        burgers: dbBurger
+      };
+
+      res.render("index", hbsObject);
+    });
+  });
+
+  // POST route
+  app.post("/", function(req, res) {
+    console.log(req.body);
+
+    db.Burger.create({
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured
+
+    }).then(function(dbBurger) {
+      // res.json(dbBurger);
+      res.redirect("/");
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+  });
+
+  // PUT route
+  app.put("/:id", function(req, res) {
+
+    console.log(req.params.id);
+
+    db.Burger.update({
+      devoured: req.body.devoured
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbBurger) {
+      res.redirect("/");
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+  });
+};
+
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki Burger', false);
